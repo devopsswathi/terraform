dfef9b06cd3bac1b05bed09ef1c0ff6e1618eced
devopsswathi <devopswithaws1223@gmail.com>
2024-11-18 15:50:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,6 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-0166fe664262f664c"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "dev-server"
-  }
+ami = "ami-03eb6185d756497f8"
+instance_type = "t2.micro"
 }
